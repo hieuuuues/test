b7d36e30d4847bfccde8fefc3c19382fde0e1f1a
Estudy <162039034+hieuuuues@users.noreply.github.com>
2025-02-09 19:33:55 +0700

    Add files via upload

---
+++ protection.js
@@ -0,0 +1,517 @@
+function _0x5edf() {
+  const _0xeb800b = [
+    "/bʊk/",
+    "wrong",
+    "show",
+    "quiz-mode",
+    "querySelectorAll",
+    "preventDefault",
+    "disabled",
+    "Apple",
+    "lang",
+    "cancel",
+    "</div>\x0a\x20\x20</div>\x0a",
+    "block",
+    "https://images.unsplash.com/photo-1517849845537-4d257902454a?w=150&h=150&fit=crop",
+    "Speech\x20synthesis\x20failed:",
+    "onclick",
+    "Bird",
+    "Phone",
+    "keydown",
+    ".card-back\x20.meaning",
+    "start-game",
+    ".card",
+    "className",
+    ".card-front\x20.audio-btn",
+    "createElement",
+    "792323duMlbj",
+    "813330owdPjm",
+    "flashcard",
+    "quiz",
+    ".quiz-option",
+    ".card-front\x20.pronunciation",
+    "word",
+    "speak",
+    "\x0a\x20\x20<img\x20src=\x22",
+    "284156OmYOWY",
+    "Book",
+    "stopPropagation",
+    "display",
+    ".card-front\x20.word",
+    "remove",
+    "Table",
+    "929714pLGLVl",
+    "8hhubCO",
+    "visibility",
+    "values",
+    "length",
+    "1269408kabXvl",
+    "getElementById",
+    "/ˈæp.əl/",
+    "Quả\x20táo",
+    ".word-info",
+    "flipped",
+    "meaning",
+    "none",
+    "key",
+    "question",
+    ".quiz-next",
+    "18111186MRZuKJ",
+    "frontWord",
+    "addEventListener",
+    "991098cDXavH",
+    "Cat",
+    "appendChild",
+    "/kæt/",
+    "pronunciation-mode",
+    ".file-btn",
+    "active",
+    "backMeaning",
+    "12TCkhhP",
+    "F12",
+    "/fɪʃ/",
+    "floor",
+    "Con\x20mèo",
+    "Quyển\x20sách",
+    "textContent",
+    "add",
+    "ctrlKey",
+    "\x22\x20onerror=\x22this.src=\x27",
+    "Fish",
+    "\x22\x20alt=\x22",
+    "frontPron",
+    "/ˈel.ɪ.fənt/",
+    "/dɒɡ/",
+    ".quiz-question",
+    "visible",
+    "backAudioBtn",
+    "style",
+    ".file-btn:nth-child(1)",
+    "warn",
+    "Elephant",
+    "hidden",
+    "en-US",
+    "mouseenter",
+    ".word-card",
+    ".file-btn:nth-child(3)",
+    "correct",
+    "querySelector",
+    "classList",
+    "forEach",
+    "pronunciation",
+    "backPron",
+    "options",
+    "Orange",
+    "data:image/svg+xml,",
+    "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=150&h=150&fit=crop",
+    "Chức\x20năng\x20này\x20bị\x20vô\x20hiệu\x20hóa!",
+    "Pen",
+    "click",
+    ".score-display",
+    "contextmenu",
+    "Grape",
+    "Danh\x20từ",
+  ];
+  _0x5edf = function () {
+    return _0xeb800b;
+  };
+  return _0x5edf();
+}
+const _0x3696eb = _0x5c96;
+(function (_0x4a3521, _0x27d80e) {
+  const _0x1c6132 = _0x5c96,
+    _0x43692d = _0x4a3521();
+  while (!![]) {
+    try {
+      const _0x42ec51 =
+        -parseInt(_0x1c6132(0x21d)) / 0x1 +
+        -parseInt(_0x1c6132(0x216)) / 0x2 +
+        parseInt(_0x1c6132(0x230)) / 0x3 +
+        -parseInt(_0x1c6132(0x222)) / 0x4 +
+        -parseInt(_0x1c6132(0x20e)) / 0x5 +
+        (parseInt(_0x1c6132(0x238)) / 0x6) *
+          (-parseInt(_0x1c6132(0x20d)) / 0x7) +
+        (-parseInt(_0x1c6132(0x21e)) / 0x8) *
+          (-parseInt(_0x1c6132(0x22d)) / 0x9);
+      if (_0x42ec51 === _0x27d80e) break;
+      else _0x43692d["push"](_0x43692d["shift"]());
+    } catch (_0x3c6193) {
+      _0x43692d["push"](_0x43692d["shift"]());
+    }
+  }
+})(_0x5edf, 0x89d34);
+const flashcardData = [
+    {
+      word: "Apple",
+      pronunciation: _0x3696eb(0x224),
+      meaning: "Quả\x20táo",
+      type: _0x3696eb(0x1f4),
+    },
+    {
+      word: _0x3696eb(0x217),
+      pronunciation: "/bʊk/",
+      meaning: "Quyển\x20sách",
+      type: _0x3696eb(0x1f4),
+    },
+    {
+      word: _0x3696eb(0x231),
+      pronunciation: _0x3696eb(0x233),
+      meaning: _0x3696eb(0x23c),
+      type: "Danh\x20từ",
+    },
+  ],
+  quizData = [
+    {
+      question: _0x3696eb(0x23c),
+      options: ["Dog", _0x3696eb(0x231), _0x3696eb(0x204), _0x3696eb(0x242)],
+      correct: 0x1,
+    },
+    {
+      question: _0x3696eb(0x23d),
+      options: [
+        _0x3696eb(0x25e),
+        _0x3696eb(0x205),
+        _0x3696eb(0x217),
+        _0x3696eb(0x21c),
+      ],
+      correct: 0x2,
+    },
+    {
+      question: _0x3696eb(0x225),
+      options: [_0x3696eb(0x25a), "Banana", _0x3696eb(0x1f3), _0x3696eb(0x1fc)],
+      correct: 0x3,
+    },
+  ],
+  pronunciationData = [
+    {
+      word: _0x3696eb(0x1fc),
+      pronunciation: _0x3696eb(0x224),
+      image:
+        "https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?w=150&h=150&fit=crop",
+    },
+    {
+      word: _0x3696eb(0x217),
+      pronunciation: _0x3696eb(0x1f5),
+      image:
+        "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?w=150&h=150&fit=crop",
+    },
+    { word: "Cat", pronunciation: _0x3696eb(0x233), image: _0x3696eb(0x25c) },
+    { word: "Dog", pronunciation: _0x3696eb(0x246), image: _0x3696eb(0x201) },
+    {
+      word: _0x3696eb(0x24d),
+      pronunciation: _0x3696eb(0x245),
+      image:
+        "https://images.unsplash.com/photo-1557050543-4d5f4e07ef46?w=150&h=150&fit=crop",
+    },
+    {
+      word: "Fish",
+      pronunciation: _0x3696eb(0x23a),
+      image:
+        "https://images.unsplash.com/photo-1524704796725-9fc3044a58b2?w=150&h=150&fit=crop",
+    },
+  ];
+let currentIndex = 0x0,
+  currentMode = "flashcard",
+  currentQuizIndex = 0x0,
+  isGameMode = ![],
+  currentGameWord = null,
+  gameScore = 0x0,
+  totalGameQuestions = 0x0;
+function playAudio(_0x7dceca) {
+  const _0x332d3d = _0x3696eb;
+  if (!_0x7dceca) return;
+  try {
+    const _0x402191 = new SpeechSynthesisUtterance(_0x7dceca);
+    (_0x402191[_0x332d3d(0x1fd)] = _0x332d3d(0x24f)),
+      (_0x402191["rate"] = 0.8),
+      speechSynthesis[_0x332d3d(0x1fe)](),
+      speechSynthesis[_0x332d3d(0x214)](_0x402191);
+  } catch (_0xd88169) {
+    console[_0x332d3d(0x24c)](_0x332d3d(0x202), _0xd88169);
+  }
+}
+function updateCard() {
+  const _0x194134 = _0x3696eb;
+  (currentIndex < 0x0 || currentIndex >= flashcardData[_0x194134(0x221)]) &&
+    (currentIndex = 0x0);
+  const _0x24b43c = flashcardData[currentIndex];
+  if (!_0x24b43c) return;
+  const _0x3103ea = {
+    frontWord: document["querySelector"](_0x194134(0x21a)),
+    frontPron: document[_0x194134(0x254)](_0x194134(0x212)),
+    backMeaning: document[_0x194134(0x254)](_0x194134(0x207)),
+    backPron: document["querySelector"](".card-back\x20.pronunciation"),
+    frontAudioBtn: document[_0x194134(0x254)](_0x194134(0x20b)),
+    backAudioBtn: document[_0x194134(0x254)](".card-back\x20.audio-btn"),
+  };
+  if (_0x3103ea[_0x194134(0x22e)])
+    _0x3103ea[_0x194134(0x22e)]["textContent"] = _0x24b43c[_0x194134(0x213)];
+  if (_0x3103ea[_0x194134(0x244)])
+    _0x3103ea["frontPron"][_0x194134(0x23e)] = _0x24b43c[_0x194134(0x257)];
+  if (_0x3103ea["backMeaning"])
+    _0x3103ea[_0x194134(0x237)]["textContent"] = _0x24b43c[_0x194134(0x228)];
+  if (_0x3103ea[_0x194134(0x258)])
+    _0x3103ea["backPron"][_0x194134(0x23e)] = _0x24b43c["type"];
+  [_0x3103ea["frontAudioBtn"], _0x3103ea[_0x194134(0x249)]]["forEach"](
+    (_0x4a985a) => {
+      _0x4a985a &&
+        (_0x4a985a["onclick"] = (_0x6aa136) => {
+          const _0x4eb0ef = _0x5c96;
+          _0x6aa136[_0x4eb0ef(0x218)](), playAudio(_0x24b43c[_0x4eb0ef(0x213)]);
+        });
+    }
+  );
+  const _0x31a5c8 = document[_0x194134(0x254)](_0x194134(0x209));
+  if (_0x31a5c8)
+    _0x31a5c8[_0x194134(0x255)][_0x194134(0x21b)](_0x194134(0x227));
+}
+function previousCard() {
+  const _0x2f0aa9 = _0x3696eb;
+  (currentIndex =
+    (currentIndex - 0x1 + flashcardData[_0x2f0aa9(0x221)]) %
+    flashcardData[_0x2f0aa9(0x221)]),
+    updateCard();
+}
+function nextCard() {
+  const _0xed4e28 = _0x3696eb;
+  (currentIndex = (currentIndex + 0x1) % flashcardData[_0xed4e28(0x221)]),
+    updateCard();
+}
+function updateQuiz() {
+  const _0x339e63 = _0x3696eb;
+  (currentQuizIndex < 0x0 || currentQuizIndex >= quizData[_0x339e63(0x221)]) &&
+    (currentQuizIndex = 0x0);
+  const _0x28cca8 = quizData[currentQuizIndex];
+  if (!_0x28cca8) return;
+  const _0x1c8d09 = document["querySelector"](".quiz-container");
+  if (!_0x1c8d09) return;
+  const _0x86fda = _0x1c8d09[_0x339e63(0x254)](_0x339e63(0x247));
+  if (_0x86fda) _0x86fda[_0x339e63(0x23e)] = _0x28cca8[_0x339e63(0x22b)];
+  const _0x34f595 = _0x1c8d09[_0x339e63(0x1f9)](_0x339e63(0x211));
+  _0x34f595[_0x339e63(0x256)]((_0x542eee, _0x28965e) => {
+    const _0x33cb53 = _0x339e63;
+    _0x28965e < _0x28cca8[_0x33cb53(0x259)][_0x33cb53(0x221)] &&
+      ((_0x542eee[_0x33cb53(0x23e)] = _0x28cca8[_0x33cb53(0x259)][_0x28965e]),
+      (_0x542eee[_0x33cb53(0x20a)] = "quiz-option"),
+      (_0x542eee["onclick"] = () => checkAnswer(_0x28965e)));
+  });
+  const _0x66b19a = document["querySelector"](_0x339e63(0x22c));
+  if (_0x66b19a) _0x66b19a["classList"][_0x339e63(0x21b)]("show");
+}
+function checkAnswer(_0x5d335e) {
+  const _0x45ed05 = _0x3696eb,
+    _0x460694 = quizData[currentQuizIndex];
+  if (!_0x460694) return;
+  const _0xe4d0cb = document[_0x45ed05(0x1f9)](_0x45ed05(0x211));
+  _0xe4d0cb[_0x45ed05(0x256)]((_0xc18828) =>
+    _0xc18828[_0x45ed05(0x255)][_0x45ed05(0x23f)](_0x45ed05(0x1fb))
+  );
+  _0x5d335e === _0x460694["correct"]
+    ? (_0xe4d0cb[_0x5d335e]["classList"][_0x45ed05(0x23f)]("correct"),
+      playAudio(_0x460694["options"][_0x5d335e]))
+    : (_0xe4d0cb[_0x5d335e][_0x45ed05(0x255)][_0x45ed05(0x23f)](
+        _0x45ed05(0x1f6)
+      ),
+      _0xe4d0cb[_0x460694[_0x45ed05(0x253)]][_0x45ed05(0x255)]["add"](
+        "correct"
+      ));
+  const _0xb5f034 = document[_0x45ed05(0x254)](_0x45ed05(0x22c));
+  if (_0xb5f034)
+    _0xb5f034[_0x45ed05(0x255)][_0x45ed05(0x23f)](_0x45ed05(0x1f7));
+}
+function initializePronunciationMode() {
+  const _0x249c9c = _0x3696eb,
+    _0x2f07fb = document[_0x249c9c(0x254)](".pronunciation-grid");
+  if (!_0x2f07fb) return;
+  (_0x2f07fb["innerHTML"] = ""),
+    pronunciationData[_0x249c9c(0x256)]((_0x5cbd7b) => {
+      const _0x473c15 = _0x249c9c,
+        _0x51ca6e = document[_0x473c15(0x20c)]("div");
+      _0x51ca6e["className"] = "word-card";
+      const _0xb57252 =
+        _0x473c15(0x25b) +
+        encodeURIComponent(
+          "\x0a\x20\x20<svg\x20xmlns=\x22http://www.w3.org/2000/svg\x22\x20width=\x22150\x22\x20height=\x22150\x22\x20viewBox=\x220\x200\x20150\x20150\x22>\x0a\x20\x20\x20\x20<rect\x20width=\x22150\x22\x20height=\x22150\x22\x20fill=\x22#f0f0f0\x22/>\x0a\x20\x20\x20\x20<text\x20x=\x2275\x22\x20y=\x2275\x22\x20text-anchor=\x22middle\x22\x20fill=\x22#999\x22>Image\x20not\x20found</text>\x0a\x20\x20</svg>\x0a"
+        );
+      (_0x51ca6e["innerHTML"] =
+        _0x473c15(0x215) +
+        _0x5cbd7b["image"] +
+        _0x473c15(0x243) +
+        _0x5cbd7b[_0x473c15(0x213)] +
+        _0x473c15(0x241) +
+        _0xb57252 +
+        "\x27\x22>\x0a\x20\x20<div\x20class=\x22word-info\x22>\x0a\x20\x20\x20\x20<div\x20class=\x22word\x22>" +
+        _0x5cbd7b[_0x473c15(0x213)] +
+        "</div>\x0a\x20\x20\x20\x20<div\x20class=\x22pronunciation\x22>" +
+        _0x5cbd7b[_0x473c15(0x257)] +
+        _0x473c15(0x1ff)),
+        _0x51ca6e[_0x473c15(0x22f)](_0x473c15(0x250), () => {
+          const _0x39fcfd = _0x473c15;
+          !isGameMode && playAudio(_0x5cbd7b[_0x39fcfd(0x213)]);
+        }),
+        _0x51ca6e[_0x473c15(0x22f)](_0x473c15(0x25f), () => {
+          const _0x125959 = _0x473c15;
+          if (isGameMode) checkGameAnswer(_0x5cbd7b[_0x125959(0x213)]);
+        }),
+        _0x2f07fb[_0x473c15(0x232)](_0x51ca6e);
+    });
+}
+function startPronunciationGame() {
+  const _0x56b307 = _0x3696eb;
+  (isGameMode = !![]), (gameScore = 0x0), (totalGameQuestions = 0x0);
+  const _0x28e71 = document["getElementById"]("start-game"),
+    _0x35f9b3 = document[_0x56b307(0x254)](_0x56b307(0x260));
+  if (_0x28e71) _0x28e71[_0x56b307(0x24a)][_0x56b307(0x219)] = _0x56b307(0x229);
+  if (_0x35f9b3)
+    _0x35f9b3[_0x56b307(0x24a)][_0x56b307(0x219)] = _0x56b307(0x200);
+  document["querySelectorAll"](_0x56b307(0x226))[_0x56b307(0x256)](
+    (_0x2f913d) => {
+      const _0x2a19fd = _0x56b307;
+      _0x2f913d[_0x2a19fd(0x24a)][_0x2a19fd(0x21f)] = _0x2a19fd(0x24e);
+    }
+  ),
+    nextGameQuestion();
+}
+function endPronunciationGame() {
+  const _0x511538 = _0x3696eb;
+  isGameMode = ![];
+  const _0x3f9794 = document["getElementById"](_0x511538(0x208)),
+    _0x1531b4 = document[_0x511538(0x254)](_0x511538(0x260));
+  if (_0x3f9794) _0x3f9794[_0x511538(0x24a)][_0x511538(0x219)] = "block";
+  if (_0x1531b4) _0x1531b4[_0x511538(0x24a)][_0x511538(0x219)] = "none";
+  document[_0x511538(0x1f9)](".word-info")[_0x511538(0x256)]((_0x1f2e1d) => {
+    const _0x361edb = _0x511538;
+    _0x1f2e1d["style"][_0x361edb(0x21f)] = _0x361edb(0x248);
+  });
+}
+function nextGameQuestion() {
+  const _0x11e98d = _0x3696eb;
+  if (!pronunciationData["length"]) return;
+  const _0x340d2f = Math[_0x11e98d(0x23b)](
+    Math["random"]() * pronunciationData[_0x11e98d(0x221)]
+  );
+  (currentGameWord = pronunciationData[_0x340d2f]),
+    currentGameWord &&
+      (playAudio(currentGameWord[_0x11e98d(0x213)]),
+      totalGameQuestions++,
+      updateGameScore());
+}
+function _0x5c96(_0x22b341, _0x587fde) {
+  const _0x5edf79 = _0x5edf();
+  return (
+    (_0x5c96 = function (_0x5c96b2, _0x1d069a) {
+      _0x5c96b2 = _0x5c96b2 - 0x1f2;
+      let _0x1deb2f = _0x5edf79[_0x5c96b2];
+      return _0x1deb2f;
+    }),
+    _0x5c96(_0x22b341, _0x587fde)
+  );
+}
+function checkGameAnswer(_0xa6dbfa) {
+  const _0x4fc72a = _0x3696eb;
+  if (!isGameMode || !currentGameWord) return;
+  const _0x4feed2 = document[_0x4fc72a(0x1f9)](_0x4fc72a(0x251));
+  _0x4feed2[_0x4fc72a(0x256)]((_0x39d70b) => {
+    const _0x519821 = _0x4fc72a,
+      _0xd1c572 = _0x39d70b[_0x519821(0x254)](".word");
+    _0xd1c572?.[_0x519821(0x23e)] === _0xa6dbfa &&
+      (_0xa6dbfa === currentGameWord[_0x519821(0x213)]
+        ? (_0x39d70b[_0x519821(0x255)]["add"](_0x519821(0x253)), gameScore++)
+        : _0x39d70b[_0x519821(0x255)]["add"](_0x519821(0x1f6)));
+  }),
+    updateGameScore(),
+    setTimeout(() => {
+      const _0x218794 = _0x4fc72a;
+      _0x4feed2[_0x218794(0x256)]((_0x3aa18f) =>
+        _0x3aa18f[_0x218794(0x255)][_0x218794(0x21b)]("correct", "wrong")
+      ),
+        totalGameQuestions >= pronunciationData[_0x218794(0x221)]
+          ? endPronunciationGame()
+          : nextGameQuestion();
+    }, 0x3e8);
+}
+function updateGameScore() {
+  const _0x591993 = _0x3696eb,
+    _0xaa5d5f = document[_0x591993(0x223)]("score"),
+    _0x10fc96 = document[_0x591993(0x223)]("total-questions");
+  if (_0xaa5d5f) _0xaa5d5f["textContent"] = gameScore;
+  if (_0x10fc96) _0x10fc96[_0x591993(0x23e)] = totalGameQuestions;
+}
+function changeMode(_0x404d6d) {
+  const _0x2a528a = _0x3696eb;
+  (currentMode = _0x404d6d),
+    document[_0x2a528a(0x1f9)](_0x2a528a(0x235))["forEach"]((_0x4b1a80) =>
+      _0x4b1a80[_0x2a528a(0x255)][_0x2a528a(0x21b)](_0x2a528a(0x236))
+    );
+  const _0x4670d7 = {
+    flashcard: document[_0x2a528a(0x223)]("flashcard-mode"),
+    quiz: document[_0x2a528a(0x223)](_0x2a528a(0x1f8)),
+    pronunciation: document[_0x2a528a(0x223)](_0x2a528a(0x234)),
+  };
+  Object[_0x2a528a(0x220)](_0x4670d7)[_0x2a528a(0x256)]((_0x5af30e) => {
+    const _0x3bfb41 = _0x2a528a;
+    if (_0x5af30e)
+      _0x5af30e[_0x3bfb41(0x24a)][_0x3bfb41(0x219)] = _0x3bfb41(0x229);
+  });
+  switch (_0x404d6d) {
+    case _0x2a528a(0x20f):
+      if (_0x4670d7[_0x2a528a(0x20f)])
+        _0x4670d7[_0x2a528a(0x20f)][_0x2a528a(0x24a)]["display"] =
+          _0x2a528a(0x200);
+      document[_0x2a528a(0x254)](_0x2a528a(0x24b))?.[_0x2a528a(0x255)][
+        _0x2a528a(0x23f)
+      ](_0x2a528a(0x236));
+      break;
+    case "quiz":
+      _0x4670d7[_0x2a528a(0x210)] &&
+        ((_0x4670d7[_0x2a528a(0x210)][_0x2a528a(0x24a)][_0x2a528a(0x219)] =
+          _0x2a528a(0x200)),
+        updateQuiz());
+      document[_0x2a528a(0x254)](".file-btn:nth-child(2)")?.[_0x2a528a(0x255)][
+        _0x2a528a(0x23f)
+      ](_0x2a528a(0x236));
+      break;
+    case 0x3:
+      _0x4670d7[_0x2a528a(0x257)] &&
+        ((_0x4670d7[_0x2a528a(0x257)]["style"]["display"] = _0x2a528a(0x200)),
+        initializePronunciationMode());
+      document[_0x2a528a(0x254)](_0x2a528a(0x252))?.[_0x2a528a(0x255)][
+        _0x2a528a(0x23f)
+      ](_0x2a528a(0x236));
+      break;
+  }
+}
+document["addEventListener"]("DOMContentLoaded", () => {
+  const _0x5773a0 = _0x3696eb,
+    _0x23b26d = document["querySelector"](_0x5773a0(0x209));
+  _0x23b26d &&
+    _0x23b26d[_0x5773a0(0x22f)](_0x5773a0(0x25f), () =>
+      _0x23b26d[_0x5773a0(0x255)]["toggle"](_0x5773a0(0x227))
+    );
+  const _0x3f1df8 = document[_0x5773a0(0x223)]("start-game");
+  _0x3f1df8 &&
+    _0x3f1df8[_0x5773a0(0x22f)](_0x5773a0(0x25f), startPronunciationGame);
+  const _0xf21968 = document["querySelector"](_0x5773a0(0x22c));
+  _0xf21968 &&
+    (_0xf21968[_0x5773a0(0x203)] = () => {
+      const _0x4dcc9f = _0x5773a0;
+      (currentQuizIndex =
+        (currentQuizIndex + 0x1) % quizData[_0x4dcc9f(0x221)]),
+        updateQuiz();
+    }),
+    updateCard(),
+    updateQuiz(),
+    initializePronunciationMode();
+}),
+  document[_0x3696eb(0x22f)](_0x3696eb(0x1f2), function (_0x239b98) {
+    const _0x5cba72 = _0x3696eb;
+    _0x239b98[_0x5cba72(0x1fa)]();
+  }),
+  document[_0x3696eb(0x22f)](_0x3696eb(0x206), function (_0x22fbbc) {
+    const _0x23cce5 = _0x3696eb;
+    (_0x22fbbc["key"] === _0x23cce5(0x239) ||
+      (_0x22fbbc[_0x23cce5(0x240)] &&
+        _0x22fbbc["shiftKey"] &&
+        _0x22fbbc[_0x23cce5(0x22a)] === "I")) &&
+      (_0x22fbbc[_0x23cce5(0x1fa)](), alert(_0x23cce5(0x25d)));
+  });
